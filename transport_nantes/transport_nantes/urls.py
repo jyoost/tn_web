20b3ec3e76f410d05ae858284e86da89800b3cfa
Alexandre BENARD <benard.al@gmail.com>
2020-11-26 18:13:09 +0100

    first add of password reset

--- transport_nantes/transport_nantes/urls.py
+++ transport_nantes/transport_nantes/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from asso_tn.views import MainTransportNantes
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', MainTransportNantes.as_view(), name='index'),
@@ -34,4 +35,14 @@ urlpatterns = [
     path('tn/', include('asso_tn.urls')),
     path('v/', include('velopolitain.urls')),
     path('vo/', include('velopolitain_observatoire.urls')),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), 
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), 
+        name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'), 
+        name='password_reset_complete'),
 ]
