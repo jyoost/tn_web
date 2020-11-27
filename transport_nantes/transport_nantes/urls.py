2110cd1775a54b5f396d3b20f76f03c3e19791ee
Alexandre BENARD <benard.al@gmail.com>
2020-11-27 14:35:05 +0100

    Change settings.py to get local dev mail server

--- transport_nantes/transport_nantes/urls.py
+++ transport_nantes/transport_nantes/urls.py
@@ -37,12 +37,15 @@ urlpatterns = [
     path('vo/', include('velopolitain_observatoire.urls')),
 
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html'), 
+        template_name='registration/password_reset_done.html'),
         name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), 
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt'),
         name='password_reset'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html'), 
+        template_name='registration/password_reset_complete.html'),
         name='password_reset_complete'),
 ]
